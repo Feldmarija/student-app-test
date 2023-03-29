c35fe777a58c184d1b2a90cc638370939f03c7b7
Feldmarija <marija.feldmane@inbox.lv>
2023-03-29 17:19:48 +0300

    [master]: Classroom progress 20032023

--- src/test/java/StudentAppTest.java
+++ src/test/java/StudentAppTest.java
@@ -1,13 +1,39 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class StudentAppTest {
-    @Test
-    public void openStudentApp() {
-        WebDriver driver = new ChromeDriver();
-        driver.get("http://acodemystudentapp-env.eba-d2vctp4d.eu-north-1.elasticbeanstalk.com/");
+
+    WebDriver driver;
+    private final String APP_URL = "http://acodemystudentapp-env.eba-d2vctp4d.eu-north-1.elasticbeanstalk.com/";
+
+    @BeforeMethod
+    public void initialise() {
+        driver = new ChromeDriver();
+        driver.get(APP_URL);
+    }
+    @AfterMethod
+    public void tearDown() {
         driver.close();
         driver.quit();
+    }
+    @Test
+    public void openStudentApp() {
+
+        WebElement addStudentButton = driver.findElement(By.xpath("//div[@class='ant-table-title']//button"));
+        addStudentButton.click();
+
+        WebElement nameField = driver.findElement(By.id("name"));
+        nameField.sendKeys("Jane Doe");
+
+        WebElement emailField = driver.findElement(By.id("email"));
+        emailField.sendKeys("siroj18841@marikuza.com");
+
+        WebElement submitButton = driver.findElement(By.xpath("//div[@class='ant-form-item-control-input-content']//button"));
+        submitButton.click();
     }
 }
